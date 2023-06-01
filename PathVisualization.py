f9b5da084b011e4b74f787130e2d5eccccd0bcd0
JasonBMa <JasonBMa@users.noreply.github.com>
2023-06-01 05:28:53 -0700

    Moved some Display class into it's own file, allow for a cleaner main, created position validation inorder to stop out of bound accesses when interacting with 2d Grid Array

--- PathVisualization.py
+++ PathVisualization.py
@@ -3,7 +3,7 @@ import sys
 import math
 from sys import exit
 from Board import Board
-
+from Display import Display
 
 # Set up the colors
 BLACK = (0, 0, 0)
@@ -13,63 +13,25 @@ DARK_GRAY = (105,105,105)
 GREEN = (0,250,154)
 RED = (220,20,60)
 BLUE = (135,206,235)
-#draw all our elements
-    #update everything
+
 ratio = [4,3] # Used to scale the UI
-size = 200 #Recommended size 80 - 100
-CELL_SIZE = 10
+size = 300 #Recommended size 80 - 100
+CELL_SIZE = 80
 width = ratio[0] * size
 height = ratio[1] * size
-pygame.init()
-screen = pygame.display.set_mode((width,height))
-pygame.display.set_caption('Path Visualizer')
+
 clock = pygame.time.Clock()
 '''
 Board Class, used to keep track of the current grid layout,
 mainly used so we can do algorithims on
 is updated every frame 1/60 of a second
 '''
-
-class Display:
-    def __init__(self,height,width):
-        self.height = height
-        self.width = width
-        self.offsetX = 30
-        self.offsetY = 30
-
-    def setup():
-        screen.fill(LIGHT_GRAY)
-
-    def getColor(value):
-        if(value == 0):
-            return WHITE
-        elif(value == 1):
-            return DARK_GRAY
-        elif(value == 2):
-            return BLUE
-        elif(value == 3):
-            return GREEN
-        elif(value == 4):
-            return RED
-
-    def drawGrid(self,board):
-        grid = board.grid
-        for row in range(board.ROW):
-            for col in range(board.COL):
-                pygame.draw.rect(screen, Display.getColor(grid[row][col]), (col * CELL_SIZE + self.offsetX, row * CELL_SIZE + self.offsetY, CELL_SIZE, CELL_SIZE),3)
-
-    def returnGrid():
-        grid = None
-        return grid
-
-
-
-board = Board(ratio[0],ratio[1])
+board = Board(ratio[0]*3,ratio[1]*3)
 display = Display(height,width)
-Display.setup()
 board.printBoard()
 running = True
 while running:
+    #Recieve user input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -78,28 +40,19 @@ while running:
             # Get the mouse position
             pos = pygame.mouse.get_pos()
             # Convert the mouse position to board coordinates
-            print(pos, "grabbed from cursor")
-            print("Limit for X Coordinate of the Cursor is between", display.offsetX,"and",display.offsetX+(ratio[0]*size))
-            print("Limit for Y Coordinate of the Cursor is between", display.offsetY,"and",display.offsetY+(ratio[1]*size))
-            print(pos)
-            # if(display.offsetX < pos[0] and pos[0] <= (display.offsetX+(ratio[0]*size))):
-            #     print("Not Valid")
-            #     continue
 
-            # if(display.offsetY < pos[1] and pos[1] <= (display.offsetY+(ratio[1]*size))):
-            #     print("Not Valid")
-            #     continue
+            display.validPosition(ratio,pos)
 
             col = (pos[0] - display.offsetX) // CELL_SIZE
             row = (pos[1] - display.offsetY) // CELL_SIZE
 
-            #print("(",col,",",row,")", "grid coordinate")
+            # print("(",col,",",row,")", "grid coordinate")
             # Update the board
             board.updateCell(row, col)
         elif event.type == pygame.K_SPACE:
             print("Begin Search")
             #Sort Algorithim
-
+    #Print out the updated grid at the end, should occur 60 times a second creating a 60 fps display.
     display.drawGrid(board)
     pygame.display.update()
     clock.tick(60)
