dc28b51ab4a8a2029a47ae007b08bdf38ff19681
JasonBMa <JasonBMa@users.noreply.github.com>
2023-06-02 07:05:26 -0700

    Switched from storing integers into storing a Cell object at each cell in the grid (2D array), Cell Object stores it's color, position

--- Board.py
+++ Board.py
@@ -1,22 +1,52 @@
+'''
+Board Class, used to keep track of the current grid layout,
+mainly used so we can do algorithims on
+is updated every frame 1/60 of a second
+'''
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+LIGHT_GRAY = (220,220,220)
+DARK_GRAY = (105,105,105)
+GREEN = (0,250,154)
+RED = (220,20,60)
+BLUE = (135,206,235)
+
+class Cell:
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+
+    def changeToBlue(self):
+        self.color = BLUE
+
+    def changeColor(self, color):
+        self.color = color
+
 class Board:
     def __init__(self, col, row):
         self.ROW = row
         self.COL = col
-        self.grid = [[0 for x in range(col)]for y in range(row)]
-        self.grid[0][0] = 1
+        self.grid = []
+        for i in range(row):
+            self.grid.append([])
+            for j in range(col):
+                cell = Cell(i,j,WHITE)
+                self.grid[i].append(cell)
 
-    def printBoard(self):
-        for i in range(self.ROW):
-            for j in range(self.COL):
-                print(self.grid[i][j] ,end=" ")
-
-            print()
+        self.start=[0,0]
+        cell = self.grid[0][0]
+        cell.color = GREEN
+        cell = self.grid[row-1][col-1]
+        cell.color = RED
+        self.endFound = False
 
     def loadBoard(self, updatedGrid):
         self.grid = updatedGrid
 
     def updateCell(self,row,col):
-        if (self.grid[row][col] == 0): # gray to dark gray
-            self.grid[row][col] = 1
-        elif (self.grid[row][col] == 1): # dark gray to gray
-            self.grid[row][col] = 0
+        cell = self.grid[row][col]
+        if (cell.color == WHITE): # gray to dark gray
+            cell.color = LIGHT_GRAY
+        elif (cell.color == LIGHT_GRAY): # dark gray to gray
+            cell.color = WHITE
